586aef4fc12ef0b9a99bfbe7b4f1d9fd83121f99
vanyut <147979342+vanyut@users.noreply.github.com>
2023-10-28 06:31:52 +0000

    111

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-number = int(input("user"))
-a1 = number %10
-a2 = number // 10%10
-a3 = number // 100%10
-a4 = number // 1000%10
-a5 = number // 10000%10
-a6 = number // 100000
-if (a1+a2+a3) == (a4+a5+a6)
-    print("happy")
-else:
-    print("unhappy")
+def get_even(number):
+    if number % 2 == 0:
+        return True
+    return False
     
+print(get_even(7))
+print(get_even(26))
 
 
-number = int(input("User"))
+def counter_symbol(s):
+    temp = "aeiou"
+    s = s.lower()
+    count_lit = 0
+    for lit in temp:
+        count_lit += s.count(lit)
+    return count_lit
 
-if len(str(number)) == 6:
-    a1 = number %10
-    a2 = number // 10%10
-    a3 = number // 100%10
-    a4 = number // 1000%10
-    a5 = number // 10000%10
-    a6 = number // 100000
-    if (a1+a2+a3) == (a4+a5+a6):
-        print("happy")
-    else:
-        print("unhappy")
-else:
-    print("invalid")
+s = "Weather in Odesa for today accurate weather forecast for today for Odesa Odeskyi district Odessa Oblast Ukraine"
+print(counter_symbol(s))
+
+
+def get_factorial(number):
+    factorial = 1
+    if number == 0:
+        print("error")
+    while number > 0:
+        factorial *= number
+        number -= 1
+    return factorial
+
+print(get_factorial(6))
